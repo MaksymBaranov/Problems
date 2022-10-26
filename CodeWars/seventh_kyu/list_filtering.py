d0f9f672854d851c40251370779b86d1c23e9834
MaksymBaranov <maksym_baranov@LAPTOP-6THQVH2E.localdomain>
2022-10-26 15:14:26 +0200

    Solved list filtering prorblem.

---
+++ CodeWars/seventh_kyu/list_filtering.py
@@ -0,0 +1,8 @@
+def filter_list(l):
+    'return a new list with the strings filtered out'
+    return [elm for elm in l if isinstance(elm, int)]
+
+
+print(filter_list([1, 2, 'a', 'b']))
+print(filter_list([1, 'a', 'b', 0, 15]))
+print(filter_list([1, 2, 'aasf', '1', '123', 123]))
