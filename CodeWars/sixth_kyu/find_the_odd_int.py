bc2f89ef60eb1e2df77fd727c59a3bb3db857bab
MaksymBaranov <maksym_baranov@LAPTOP-6THQVH2E.localdomain>
2022-10-27 10:57:16 +0200

    find_the_odd_integers_problem_solved_needs_refactoring

---
+++ CodeWars/sixth_kyu/find_the_odd_int.py
@@ -0,0 +1,21 @@
+'''
+https://www.codewars.com/kata/54da5a58ea159efa38000836/train/python
+'''
+
+
+def find_it(seq):
+    numbers_count = {}
+    for elm in seq:
+        numbers_count[elm] = numbers_count.get(elm, 0) + 1
+    for key, val in numbers_count.items():
+        if val % 2 != 0:
+            return key
+
+
+print(find_it([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]))
+print(find_it([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]))
+print(find_it([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5]))
+print(find_it([10]))
+print(find_it([10, 10, 10]))
+print(find_it([1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1]))
+print(find_it([5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10]))
